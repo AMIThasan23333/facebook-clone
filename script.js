edc7c022d52ad476386d49bf4544586498940071
My Name <cpgsom819@gmail.com>
2023-06-07 19:39:51 +0800

    color added

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ var root =document.querySelector(":root");
 console.log(root);
 
 
+const colorPalette =document.querySelectorAll(".choose-color span")
+
+
+
 
 
 
@@ -128,11 +132,25 @@ const closeThemeModal  = (e) => {
 themeModal.addEventListener('click', closeThemeModal);
 
 
-/* ============= font =========== */
+/*=============font===========*/
+
+ const removeSizeSelector = () => {
+
+   fontSizes.forEach(size => {
+    size.classList.remove('active')
+   })
+ }
 
 
 fontSizes.forEach((size) => {
+
+
+
   size.addEventListener('click', () => {
+    removeSizeSelector()
+
+    size.classList.add('active')
+
     let fontSize;
 
     if (size.classList.contains('font-size-1')) {
@@ -161,3 +179,28 @@ fontSizes.forEach((size) => {
   });
 });
 
+
+
+/* change primary color */
+
+
+
+colorPalette.forEach((color) => {
+  color.addEventListener('click', () => {
+    let primaryHue;
+
+    if (color.classList.contains('color-1')) {
+      primaryHue = 252;
+    } else if (color.classList.contains('color-2')) {
+      primaryHue = 52;
+    } else if (color.classList.contains('color-3')) {
+      primaryHue = 352;
+    } else if (color.classList.contains('color-4')) {
+      primaryHue = 152;
+    } else if (color.classList.contains('color-5')) {
+      primaryHue = 202;
+    }
+
+    root.style.setProperty('--primary-color-hue', primaryHue.toString());
+  });
+});
